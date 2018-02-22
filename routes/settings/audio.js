1358faa949b7d2a013973c5791d784c1e9e45db1
Anna <hanna.pilipenko@gmail.com>
2018-02-22 23:15:28 +0200

    add settings/audio

--- routes/settings/audio.js
+++ routes/settings/audio.js
@@ -1,14 +1,56 @@
 import express from 'express';
+import { dbManager }  from 'db';
 import devicesManager from 'devices';
+
 const router = express.Router();
-
 let PAGE_PATH = 'settings/audio';
-
 let devManager = new devicesManager();
 
+const getAudioList = async () => {
+	return await dbManager.getAudioDevices({ order: { type: 'DESC' }});
+};
+
 router.get('/', async (req, res) => {
-	console.log('AUDIO PAGE')
-	res.view(PAGE_PATH);
+	let list = await getAudioList();
+	let mediaDevices = await devManager.getMediaDevices();
+	res.view(PAGE_PATH, { result: list.result, audioDevices: mediaDevices.audio });
 });
 
+// create new audio device
+router.post('/', async (req, res) => {
+	let form = req.body;
+	console.log('FULLNAME', form.fullname);
+	let fullname = form.fullname.split('    '),
+		name = fullname[0].trim(),
+		path = fullname[1].trim().replace('(', '').replace(')','');
+
+	console.log('name', name);
+	console.log('path', path);
+
+	let out = await dbManager.createAudioDevice(name, path);
+	let list = await getAudioList();
+	let mediaDevices = await devManager.getMediaDevices();
+
+	console.log('list', list);
+
+	if (out.status == 0) {
+		res.view(PAGE_PATH, { status: 0, result: list.result, success: 'Успешно добавлено', audioDevices: mediaDevices.audio  });
+	}
+	else {
+		res.view(PAGE_PATH, { status: 1, error: 'Что-то пошло не так...', result: list.result, audioDevices: mediaDevices.audio   });
+	}
+});
+
+// remove audio device
+router.get('/:id', async (req, res) => {
+	let out = await dbManager.removeAudioDevice(req.params.id);
+	let list = await getAudioList();
+	let mediaDevices = await devManager.getMediaDevices();
+	if (out.status == 0) {
+		res.view('/' + PAGE_PATH, { status: 0, result: list.result, success: 'Успешно удалено', audioDevices: mediaDevices.audio });
+	}
+	else {
+		res.view('/' + PAGE_PATH, { status: 1, error: 'Что-то пошло не так...', result: list.result,audioDevices: mediaDevices.audio});
+	}
+});
 export default router;
--- utils/db/dbManager.js
+++ utils/db/dbManager.js
@@ -5,8 +5,69 @@ class dbManager {
 		this.db = new db();
 	}
 
+	/*		Video		*/
+
 	getVideoDevices(options = {}) {
 		let sql = 'SELECT * FROM video';
+
+		if(options.id) {
+			sql += ' WHERE id = ' + parseInt(options.id);
+		}
+
+		if(options.order) {
+		sql += ' ORDER BY ' + (options.order.by || 'id')  + ' ' + (options.order.type == 'ASC' ? 'ASC' : 'DESC');
+		}
+
+		return this.db.query(sql)
+		.then(out => {
+			if (out && options.id){
+				out = out[0];
+			}
+
+			return {status: 0, result: out};
+		})
+		.catch (err => {
+			return { status: 1, error: 'Ошибка получения списка видео: ' + err};
+		});
+	}
+
+	createVideoDevice(name, source) {
+		return this.db.query('INSERT INTO video (name, source) VALUES (?,?)', [name, source])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION createVideoDevice: ', err);
+			return({ status: 1, error: 'Ошибка добавления видео: ' + err});
+		});
+	}
+
+	removeVideoDevice(id) {
+		return this.db.query('DELETE FROM video WHERE id=?', [id])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION removeVideoDevice: ', err);
+			return({ status: 1, error: 'Ошибка удаления видео (id = ' + id + '): ' + err});
+		});
+	}
+
+	updateVideoDevice(id, name, source) {
+		return this.db.query('UPDATE video SET name=?,source=? WHERE id=?', [name, source, id])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION updateVideoDevice: ', err);
+			return({ status: 1, error: 'Ошибка изменения видео (id = ' + id + '): ' + err});
+		});
+	}
+
+	/*		Audio		*/
+
+	getAudioDevices(options = {}) {
+		let sql = 'SELECT * FROM audio';
 
 		if(options.id) {
 			sql += ' WHERE id = ' + parseInt(options.id);
@@ -17,51 +78,51 @@ class dbManager {
 		}
 
 		return this.db.query(sql)
-			.then(out => {
-				if (out && options.id){
-					out = out[0];
-				}
-				
-				return {status: 0, result: out};
-			})
-			.catch (err => {
-				console.log('EXCEPTION getVideoDevices: ', err);
-				return { status: 1, error: 'Ошибка получения списка видео: ' + err};
-			});
+		.then(out => {
+			if (out && options.id){
+				out = out[0];
+			}
+
+			return {status: 0, result: out};
+		})
+		.catch (err => {
+			console.log('EXCEPTION getAudioDevices: ', err);
+			return { status: 1, error: 'Ошибка получения списка аудио: ' + err};
+		});
 	}
 
-	  createVideoDevice(name, source) {
-  		return this.db.query('INSERT INTO video (name, source) VALUES (?,?)', [name, source])
-			.then(out => {
-				return({ status: 0});
-			})
-			.catch(err => {
-				console.log('EXCEPTION createVideoDevice: ', err);
-				return({ status: 1, error: 'Ошибка добавления видео: ' + err});
-			});
-	  }
+	createAudioDevice(name, path) {
+		return this.db.query('INSERT INTO audio (name, path) VALUES (?,?)', [name, path])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION createAudioDevice: ', err);
+			return({ status: 1, error: 'Ошибка добавления аудио: ' + err});
+		});
+	}
 
-	  removeVideoDevice(id) {
-  		return this.db.query('DELETE FROM video WHERE id=?', [id])
-			.then(out => {
-				return({ status: 0});
-			})
-			.catch(err => {
-				console.log('EXCEPTION removeVideoDevice: ', err);
-				return({ status: 1, error: 'Ошибка удаления видео (id = ' + id + '): ' + err});
-			});
-	  }
+	removeAudioDevice(id) {
+		return this.db.query('DELETE FROM audio WHERE id=?', [id])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION removeAudioDevice: ', err);
+			return({ status: 1, error: 'Ошибка удаления аудио (id = ' + id + '): ' + err});
+		});
+	}
 
-	  updateVideoDevice(id, name, source) {
-	  		return this.db.query('UPDATE video SET name=?,source=? WHERE id=' + id, [name, source])
-				.then(out => {
-					return({ status: 0});
-				})
-				.catch(err => {
-					console.log('EXCEPTION updateVideoDevice: ', err);
-					return({ status: 1, error: 'Ошибка изменения видео (id = ' + id + '): ' + err});
-				});
-	  }
+	updateAudioDevice(id, name, path) {
+		return this.db.query('UPDATE audio SET name=?,path=? WHERE id=?', [name, path, id])
+		.then(out => {
+			return({ status: 0});
+		})
+		.catch(err => {
+			console.log('EXCEPTION updateAudioDevice: ', err);
+			return({ status: 1, error: 'Ошибка изменения аудио (id = ' + id + '): ' + err});
+		});
+	}
 }
 
 let manager = new dbManager();
