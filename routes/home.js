d8d6569e384c3e6c2b1fa7920cc4819bd65d110a
Anna <hanna.pilipenko@gmail.com>
2018-02-23 21:31:44 +0200

    change home

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import { dbManager }  from 'db';
 
 const router = express.Router();
 
+let PAGE_PATH = 'home';
+
+const getVideoList = async () => {
+	return await dbManager.getVideoDevices({ order: { type: 'DESC' }});
+};
+
+const getAudioList = async () => {
+	return await dbManager.getAudioDevices({ order: { type: 'DESC' }});
+};
+
 // index
-router.get('/', (req, res) => {
-	res.view();
+router.get('/', async(req, res) => {
+	let video = await getVideoList();
+	let audio = await getAudioList();
+	res.view(PAGE_PATH, { videoDevices: video.result, audioDevices: audio.result });
 });
 
 export default router;
